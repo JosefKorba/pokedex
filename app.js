f7b17751da64da4bf1ee23d44a902901ec319650
JosefKorba <josef.korba1@gmail.com>
2024-03-11 23:23:43 +0100

    Refactor code with Prettier

--- app.js
+++ app.js
@@ -11,41 +11,44 @@ const displayPokemon = (pokemon) => {
   const pokemonArray = Array.isArray(pokemon) ? pokemon : [pokemon];
 
   const pokemonHTMLString = pokemonArray
-    .map(
-      (pal) => {
+    .map((pal) => {
+      const firstLetter = pal.name.charAt(0).toUpperCase();
+      const firstLetterCaps = firstLetter + pal.name.slice(1);
 
-    const firstLetter = pal.name.charAt(0).toUpperCase();
-    const firstLetterCaps = firstLetter + pal.name.slice(1);
-
-    return `<li class="main__card">
+      return `<li class="main__card">
                 <img class="main__card-image" src="${pal.sprites.front_default}"/>
                 <h2 class="main__card-title">${pal.id}. ${firstLetterCaps}</h2>
-            </li>`
-        })
+            </li>`;
+    })
     .join("");
   pokedex.insertAdjacentHTML("beforeend", pokemonHTMLString);
 };
 
 // Fetch data for the first 151 Pokemon
-const pokemonPromises = Array.from({ length: 151 }, (_, i) => catchPokemon(i + 1));
+const pokemonPromises = Array.from({ length: 151 }, (_, i) =>
+  catchPokemon(i + 1)
+);
 
 // Display all fetched Pokemon data
-Promise.all(pokemonPromises).then(pokemonArray => {
-    displayPokemon(pokemonArray);
+Promise.all(pokemonPromises).then((pokemonArray) => {
+  displayPokemon(pokemonArray);
 });
 
-
 // Search pokemon
-document.getElementById('search-button').addEventListener('click', function() {
-  const searchTerm = document.querySelector('.header__search').value.toLowerCase();
-  const pokemonList = document.querySelectorAll('.main__card');
+document.getElementById("search-button").addEventListener("click", function () {
+  const searchTerm = document
+    .querySelector(".header__search")
+    .value.toLowerCase();
+  const pokemonList = document.querySelectorAll(".main__card");
 
-  pokemonList.forEach(pokemonCard => {
-      const pokemonName = pokemonCard.querySelector('.main__card-title').textContent.toLowerCase();
-      if (pokemonName.includes(searchTerm)) {
-          pokemonCard.style.display = 'block';
-      } else {
-          pokemonCard.style.display = 'none';
-      }
+  pokemonList.forEach((pokemonCard) => {
+    const pokemonName = pokemonCard
+      .querySelector(".main__card-title")
+      .textContent.toLowerCase();
+    if (pokemonName.includes(searchTerm)) {
+      pokemonCard.style.display = "block";
+    } else {
+      pokemonCard.style.display = "none";
+    }
   });
 });
